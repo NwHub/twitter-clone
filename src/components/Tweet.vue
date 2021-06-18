c5f34e3d3475405a6a93755ee0a0a86df03602a1
shiro <pengin01@gmail.com>
2021-06-18 16:05:52 +0900

    Phase2 20210618 (#1)
    * 画面テンプレート作成
    
    * カウント
    
    * ユーザー名表示
    
    Co-authored-by: “taihei” <“taihei524next@gmail.com”>
    Co-authored-by: N A <sea_mope@yahoo.co.jp>
    Co-authored-by: Sugo <syukiko72@gmail.com>

---
+++ src/components/Tweet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ user.userName }} {{ user.firstName }} {{ user.lastName }}</h1>
+    <strong>followers:</strong> {{ followers }}
+    <button @click="followerCount">follow</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tweet",
+  data: () => {
+    return {
+      followers: 0,
+      user: {
+        userName: "ミワ",
+        firstName: "泰平",
+        lastName: "三輪"
+      }
+    };
+  },
+  computed: {
+    hogeComputed() {
+      return "";
+    }
+  },
+  methods: {
+    followerCount() {
+      this.followers++;
+    }
+  }
+};
+</script>
+<style>
+h1 {
+  margin: 0;
+}
+</style>
