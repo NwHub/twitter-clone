ff45d8900004201422ccd0e3e8ec46b6fee7dc5f
“taihei” <taihei524next@gmail.com>
2021-06-18 05:40:55 +0000

    画面テンプレート作成

---
+++ src/components/Tweet.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>@User - yamada taro</h1>
+    <strong>followers:</strong> 1 <button>follow</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hoge",
+  data: () => {
+    return {
+      hoge: "hoge"
+    };
+  },
+  computed: {
+    hogeComputed() {
+      return "";
+    }
+  },
+  methods: {
+    hogeMethods() {}
+  }
+};
+</script>
+<style>
+h1 {
+  margin: 0;
+}
+</style>
