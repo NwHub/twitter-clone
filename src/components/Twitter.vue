79a50924285f045ed48c7f286adddcd47dd3619b
matsuki <maaaaayu.euph@gmail.com>
2021-09-02 05:37:22 +0000

    画面テンプレート作成

---
+++ src/components/Twitter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="user-profile">
+      <div class="user-profile__user-panel">
+        <h1>@User - yamada taro</h1>
+        <div>
+          <strong>Followers: </strong> 1
+          <button>follow</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hoge",
+  data: () => {
+    return {
+      hoge: "hoge"
+    };
+  },
+  methods: {
+    hogeMethods() {}
+  }
+};
+</script>
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 2fr 4fr;
+  grid-gap: 50px;
+  padding: 50px 5%;
+}
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+  margin-bottom: auto;
+}
+h1 {
+  margin: 0;
+}
+button {
+  padding: 5px 20px;
+  margin: auto 0;
+  border-radius: 5px;
+  border: none;
+  background-color: deeppink;
+  color: white;
+  font-weight: bold;
+}
+</style>
